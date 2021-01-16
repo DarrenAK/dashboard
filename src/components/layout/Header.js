14bd744206e5e9c432bb5e7c389da2ce0737de69
DarrenAK <darrenkonczak@gmail.com>
2021-01-16 15:42:07 +0100

    adding all files for backup

---
+++ src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header-wrapper">
+      <h1>My Playground</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+export const clientId = "2c86caeda6c54b4fa6a6474d98c6972a";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
